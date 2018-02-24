219ec310b60495b52702b53e07879a1d80ada80f
hville <hugov@runbox.com>
2018-02-24 18:16:16 -0500

    fix test

--- test.js
+++ test.js
@@ -1,6 +1,8 @@
 'use strict'
-var c = require('cotest')
-var q = require('./sample-quantile')
+var c = require('cotest'),
+		q = require('./'),
+		single = require('./quantile'),
+		many = require('./quantiles')
 
 var delta = 0.02
 
@@ -23,12 +25,12 @@ c('single prob', function() {
 	c('===', q([1], 0), 1 )
 	c('===', q([1], 1), 1 )
 	c('===', q([1,2], 0.5), 1.5)
-	c('===', q.single([1,2], 0.5), 1.5)
+	c('===', single([1,2], 0.5), 1.5)
 })
 c('multi probs', function() {
 	c('{==}', q([1,2], [0, 0.5, 1]), [1, 1.5, 2])
 	c('{==}', q([0,1,2,3,4,5,6], [0.25, 0.5, 0.75]), [1,3,5])
-	c('{==}', q.array([0,1,2,3,4,5,6], [0.25, 0.5, 0.75]), [1,3,5])
+	c('{==}', many([0,1,2,3,4,5,6], [0.25, 0.5, 0.75]), [1,3,5])
 })
 c('samples to be sorted', function() {
 	c('===', q([3, 1, 2], 1, true), 3)
